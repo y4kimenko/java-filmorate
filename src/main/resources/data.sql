0be730df08ef46ba7bf8dc9f2a71ac4d7c6a32ff
dmitry-udovin <dimav0406@gmail.com>
2026-03-15 14:23:43 +0300

    безопасные вставки (data.sql)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,10 @@
-INSERT INTO mpa (id, name) VALUES
-    (1, 'G'),
-    (2, 'PG'),
-    (3, 'PG-13'),
-    (4, 'R'),
-    (5, 'NC-17');
-
-INSERT INTO genres (id, name) VALUES
-    (1, 'Комедия'),
-    (2, 'Драма'),
-    (3, 'Мультфильм'),
-    (4, 'Триллер'),
-    (5, 'Документальный'),
-    (6, 'Боевик');
+INSERT INTO mpa (id, name)
+SELECT 1, 'G' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 1);
+INSERT INTO mpa (id, name)
+SELECT 2, 'PG' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 2);
+INSERT INTO mpa (id, name)
+SELECT 3, 'PG-13' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 3);
+INSERT INTO mpa (id, name)
+SELECT 4, 'R' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 4);
+INSERT INTO mpa (id, name)
+SELECT 5, 'NC-17' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 5);
