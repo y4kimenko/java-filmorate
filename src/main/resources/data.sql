60a2804af42bbd63f79be3e0c49dda3f2cbbbe24
dmitry-udovin <dimav0406@gmail.com>
2026-03-15 14:36:30 +0300

    также добавлено info о жанрах (genres)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,16 @@ INSERT INTO mpa (id, name)
 SELECT 4, 'R' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 4);
 INSERT INTO mpa (id, name)
 SELECT 5, 'NC-17' WHERE NOT EXISTS (SELECT 1 FROM mpa WHERE id = 5);
+
+INSERT INTO genres (id, name)
+SELECT 1, 'Комедия' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 1);
+INSERT INTO genres (id, name)
+SELECT 2, 'Драма' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 2);
+INSERT INTO genres (id, name)
+SELECT 3, 'Мультфильм' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 3);
+INSERT INTO genres (id, name)
+SELECT 4, 'Триллер' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 4);
+INSERT INTO genres (id, name)
+SELECT 5, 'Документальный' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 5);
+INSERT INTO genres (id, name)
+SELECT 6, 'Боевик' WHERE NOT EXISTS (SELECT 1 FROM genres WHERE id = 6);
