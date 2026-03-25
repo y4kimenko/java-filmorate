98fcf662b46dc6c64dedb9cb2cac172f4e5a1f1a
Сергей Овчинников <demix2703204@gmail.com>
2026-03-25 04:40:40 +0300

    Переделан контроллер

--- src/main/java/ru/yandex/practicum/filmorate/controller/film/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/film/FilmController.java
@@ -59,7 +59,7 @@ public class FilmController {
 
     @GetMapping("/popular")
     public List<FilmResponseDto> getPopularFilms(
-            @RequestParam
+            @RequestParam(defaultValue = "10")
             @PositiveOrZero(message = "count не может быть отрицательным")
             int count,
 
@@ -71,7 +71,7 @@ public class FilmController {
             @PositiveOrZero(message = "year не может быть отрицательным")
             Long year
     ) {
-        if (genreId != null && year != null) {
+        if (genreId != null || year != null) {
             return filmService.getMostPopularFilms(count, genreId, year);
         }
 
