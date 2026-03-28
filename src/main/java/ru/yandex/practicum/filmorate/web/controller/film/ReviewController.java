1ae309d3c3e803871886555f18efb495406815c4
Сергей Овчинников <demix2703204@gmail.com>
2026-03-28 19:20:21 +0300

    Контроллер перенесен в другую папку

--- src/main/java/ru/yandex/practicum/filmorate/web/controller/film/ReviewController.java
+++ src/main/java/ru/yandex/practicum/filmorate/web/controller/film/ReviewController.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.controller.review;
+package ru.yandex.practicum.filmorate.web.controller.film;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
