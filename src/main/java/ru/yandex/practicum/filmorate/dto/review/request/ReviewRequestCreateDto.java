77a214833d3874c5953946f6a17e40a5e39ff85f
Сергей Овчинников <demix2703204@gmail.com>
2026-03-28 19:38:25 +0300

    Исправлены ошибки №2

--- src/main/java/ru/yandex/practicum/filmorate/dto/review/request/ReviewRequestCreateDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/review/request/ReviewRequestCreateDto.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dto.review.request;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 
 public record ReviewRequestCreateDto(
         @NotNull
